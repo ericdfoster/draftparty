e70a74bcbe28082f1d5e52b4a4627682e99a2007
Eric Foster <ericdfoster@gmail.com>
2020-02-04 18:14:51 -0500

    Added a new section to the setup script that loads the draft, guesses, and pirates datasets into the global environment.

--- Code/setup.R
+++ Code/setup.R
@@ -4,6 +4,7 @@
 ###                                                                                ###
 ### DESCRIPTION: The setup script. This script will load the necessary libraries   ###
 ###              and will source the user-defined functions from the repository.   ###
+###    The setup script will also load all data into dataframes.                   ###
 ###                                                                                ###
 ### INPUTS: None                                                                   ###
 ###                                                                                ###
@@ -26,3 +27,9 @@ require(here) # Technically, this call needs to be made within the app itself.
 FP_01 <- sort(dir(path = paste0(getwd(), "/Code/"), pattern = "\\.[rR]$", full.names = FALSE))
 FP_01 <- FP_01[!grepl(pattern = "setup.R", x = FP_01)]
 lapply(X = FP_01, FUN = function(x){ source(here::here("Code", x)) })
+
+#################
+### LOAD DATA ###
+#################
+
+updateEnvironmentDatasets(action = "pull")
