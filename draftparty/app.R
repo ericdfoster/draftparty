90aa92d3162dad41c35ba6abbdb7bff798120d8f
Eric Foster <ericdfoster@gmail.com>
2020-02-06 17:57:34 -0500

    Added pirate view screen to the app.

--- draftparty/app.R
+++ draftparty/app.R
@@ -17,24 +17,34 @@ source(here("Code", "setup.R"))
 
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    ### TITLE ###
+    titlePanel(paste0(format(Sys.Date(), "%Y"), " NFL DRAFT PARTY")),
 
-    # Sidebar with a slider input for number of bins 
+    ### SIDEBAR PANEL LAYOUT ###
     sidebarLayout(
+        
+        ### SIDEBAR PANEL ###
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+
+            
+            
         ),
 
-        # Show a plot of the generated distribution
+        ### MAIN PANEL ###
         mainPanel(
-           plotOutput("distPlot")
+            
+            tabsetPanel(
+                
+                id = "mainscreens",
+                
+                tabPanel("Pirates", DT::dataTableOutput(outputId = "pirates"))
+                
+            )
+
         )
+        
     )
+    
 )
 
 ##############################
@@ -42,15 +52,13 @@ ui <- fluidPage(
 ##############################
 
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$pirates <- DT::renderDataTable({
+        
+        DT::datatable(PIRATES)
+        
     })
+
 }
 
 ###########################
