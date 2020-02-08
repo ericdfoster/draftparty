4762bb9822710500f2029d557e64a12868f39b82
Eric Foster <ericdfoster@gmail.com>
2020-02-07 20:03:43 -0500

    Moved from use of getwd to use of here::here.

--- Code/setup.R
+++ Code/setup.R
@@ -25,7 +25,7 @@ library(DT)
 ### USER-DEFINED FUNCTIONS ###
 ##############################
 
-FP_01 <- sort(dir(path = paste0(getwd(), "/Code/"), pattern = "\\.[rR]$", full.names = FALSE))
+FP_01 <- sort(dir(path = here::here("Code"), pattern = "\\.[rR]$", full.names = FALSE))
 FP_01 <- FP_01[!grepl(pattern = "setup.R", x = FP_01)]
 lapply(X = FP_01, FUN = function(x){ source(here::here("Code", x)) })
 
