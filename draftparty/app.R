89d9c736e9d75f357bf9779eaa69d345d9bf0bff
Eric Foster <ericdfoster@gmail.com>
2020-02-09 11:37:48 -0500

    Added framework for side panel selections.

--- draftparty/app.R
+++ draftparty/app.R
@@ -26,7 +26,46 @@ ui <- fluidPage(
         ### SIDEBAR PANEL ###
         sidebarPanel(
 
-            
+            tabsetPanel(
+                
+                id = "sidescreens",
+                
+                tabPanel(paste0(format(Sys.Date(), "%Y"), " Draft"),
+                         
+                         selectInput(inputId = "DRAFT_YEAR",
+                                     label = "Year:",
+                                     choices = c(2017:as.numeric(format(Sys.Date(), "%Y")))[unlist(lapply(X = 2017:as.numeric(format(Sys.Date(), "%Y")), FUN = function(x){ exists(paste0("DRAFT_", x)) }))],
+                                     selected = as.numeric(format(Sys.Date(), "%Y"))),
+                         
+                         selectInput(inputId = "DRAFT_PICK",
+                                     label = "Pick Number:",
+                                     choices = get(paste0("DRAFT_",
+                                                          format(Sys.Date(), "%Y")))$PICK),
+                         
+                         selectInput(inputId = "DRAFT_TEAM",
+                                     label = "Team Name:",
+                                     choices = nflTeams()$TEAMNAME),
+                         
+                         textInput(inputId = "DRAFT_FIRST_NAME",
+                                   label = "Player First Name:"),
+                         
+                         textInput(inputId = "DRAFT_LAST_NAME",
+                                   label = "Player Last Name:"),
+                         
+                         selectInput(inputId = "DRAFT_POSITION",
+                                     label = "Player Position:",
+                                     choices = c("C", "CB", "DE", "DT", "G", "K", "LB", "P", "QB", "RB", "S", "T", "TE", "WR")),
+                         
+                         textInput(inputId = "DRAFT_COLLEGE",
+                                   label = "Player College:"),
+                         
+                         selectInput(inputId = "DRAFT_SIDE",
+                                     label = "OFF or DEF:",
+                                     choices = c("OFF", "DEF"))
+                         
+                         )
+                
+            )
             
         ),
 
@@ -37,7 +76,7 @@ ui <- fluidPage(
                 
                 id = "mainscreens",
                 
-                tabPanel("Pirates", DT::dataTableOutput(outputId = "pirates"))
+                tabPanel("Pirates", DT::dataTableOutput(outputId = "PIRATES"))
                 
             )
 
@@ -53,7 +92,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     
-    output$pirates <- DT::renderDataTable({
+    output$PIRATES <- DT::renderDataTable({
         
         DT::datatable(PIRATES, options = list("pageLength" = 20))
         
