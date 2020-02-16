4da782401b19411d9b661c801d76cd300fa8128f
Eric Foster <ericdfoster@gmail.com>
2020-02-16 15:54:27 -0500

    Changed the setup of new drafts from a data.frame to a tibble. Also linked the sidepanel inputs of the present draft to the present draft's tibble. The user can now write to the present draft's tibble.

--- Code/setup.R
+++ Code/setup.R
@@ -35,18 +35,18 @@ lapply(X = FP_01, FUN = function(x){ source(here::here("Code", x)) })
 
 updateEnvironmentDatasets(action = "pull")
 if(!any(list.files(path = here::here("Data"), recursive = TRUE) == 
-        paste0("DRAFT_", format(Sys.Date(), "%Y")))){
+        paste0("DRAFT_", format(Sys.Date(), "%Y"), ".tsv"))){
   
   assign(x = paste0("DRAFT_", format(Sys.Date(), "%Y")),
-         value = data.frame(YEAR = rep(as.numeric(format(Sys.Date(), "%Y")), 32),
-                            PICK = 1:32,
-                            TEAM = rep("", 32),
-                            PLAYER_FIRST = rep("", 32),
-                            PLAYER_LAST = rep("", 32),
-                            POSITION = rep("", 32),
-                            COLLEGE = rep("", 32),
-                            SIDE = rep("", 32),
-                            SIDEN = rep("", 32)))
+         value = tibble(YEAR = rep(as.numeric(format(Sys.Date(), "%Y")), 32),
+                        PICK = 1:32,
+                        TEAM = rep("", 32),
+                        PLAYER_FIRST = rep("", 32),
+                        PLAYER_LAST = rep("", 32),
+                        POSITION = rep("", 32),
+                        COLLEGE = rep("", 32),
+                        SIDE = rep("", 32),
+                        SIDEN = as.numeric(rep(NA, 32))))
   updateEnvironmentDatasets(action = "push")
   
 }
--- draftparty/app.R
+++ draftparty/app.R
@@ -37,19 +37,15 @@ ui <- fluidPage(
                 
                 id = "sidescreens",
                 
-                tabPanel(paste0(format(Sys.Date(), "%Y"), " Draft"),
+                tabPanel(title = paste0(format(Sys.Date(), "%Y"), " Draft"),
                          
-                         selectInput(inputId = "DRAFT_YEAR",
-                                     label = "Year:",
-                                     choices = c(2017:as.numeric(format(Sys.Date(), "%Y")))[unlist(lapply(X = 2017:as.numeric(format(Sys.Date(), "%Y")), FUN = function(x){ exists(paste0("DRAFT_", x)) }))],
-                                     selected = as.numeric(format(Sys.Date(), "%Y"))),
-                         
-                         hr(),
+                         br(),
                          
                          selectInput(inputId = "DRAFT_PICK",
                                      label = "Pick Number:",
                                      choices = get(paste0("DRAFT_",
-                                                          format(Sys.Date(), "%Y")))$PICK),
+                                                          format(Sys.Date(), "%Y")))$PICK,
+                                     selected = 1),
                          
                          selectInput(inputId = "DRAFT_TEAM",
                                      label = "Team Name:",
@@ -70,7 +66,21 @@ ui <- fluidPage(
                          
                          selectInput(inputId = "DRAFT_SIDE",
                                      label = "OFF or DEF:",
-                                     choices = c("OFF", "DEF"))
+                                     choices = c("OFF", "DEF")),
+                         
+                         actionButton(inputId = "DRAFT_PUSH",
+                                      label = "Update Draft")
+                         
+                         ),
+                
+                tabPanel(title = "Change View",
+                         
+                         selectInput(inputId = "DRAFT_YEAR",
+                                     label = "Year:",
+                                     choices = c(2017:as.numeric(format(Sys.Date(), "%Y")))[unlist(lapply(X = 2017:as.numeric(format(Sys.Date(), "%Y")), FUN = function(x){ exists(paste0("DRAFT_", x)) }))],
+                                     selected = as.numeric(format(Sys.Date(), "%Y"))),
+                         
+                         hr(),
                          
                          )
                 
@@ -103,15 +113,60 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     
+    ### PLACE HOLDERS FOR REACTIVE OUTPUT ###
+    
+    values <- reactiveValues(DF_DRAFT_RESULTS = get(x = paste0("DRAFT_", format(Sys.Date(), "%Y"))))
+
+    ### DIRECTLY MODIFY THE OUTPUT LIST ###
+    
     output$PIRATES <- DT::renderDataTable({
         
         DT::datatable(PIRATES, options = list("pageLength" = 20))
         
     })
     
+    # BOTH PAST AND PRESENT DRAFT RESULTS #
     output$DRAFT_RESULTS <- DT::renderDataTable({
         
-        DT::datatable(get(x = paste0("DRAFT_", input$DRAFT_YEAR)), options = list("pageLength" = 32))
+        if(input$DRAFT_YEAR == format(Sys.Date(), "%Y")){
+            
+            # PAST DRAFT RESULTS #
+            DT::datatable(values$DF_DRAFT_RESULTS, options = list("pageLength" = 32))
+            
+        }else{
+            
+            # PRESENT DRAFT RESULTS #
+            DT::datatable(get(x = paste0("DRAFT_", input$DRAFT_YEAR)), options = list("pageLength" = 32))
+            
+        }
+        
+    })
+    
+    ### EVENT OBSERVATIONS ###
+    
+    # BUTTON TO RECORD A DRAFT PICK HAS BEEN PRESSED #
+    observeEvent(eventExpr = input$DRAFT_PUSH, handlerExpr = {
+        
+        # GET THE PRESENT DRAFT RESULTS #
+        TEMP_01 <- values$DF_DRAFT_RESULTS
+        
+        # RECORD THE PICK #
+        TEMP_01$TEAM[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_TEAM
+        TEMP_01$PLAYER_FIRST[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_FIRST_NAME
+        TEMP_01$PLAYER_LAST[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_LAST_NAME
+        TEMP_01$POSITION[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_POSITION
+        TEMP_01$COLLEGE[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_COLLEGE
+        TEMP_01$SIDE[as.numeric(input$DRAFT_PICK)] <- input$DRAFT_SIDE
+        TEMP_01$SIDEN[as.numeric(input$DRAFT_PICK)] <- ifelse(input$DRAFT_SIDE == "DEF", 1, 0)
+        
+        # OVERWRITE THE OLD DRAFT RESULTS WITH THE UPDATED DRAFT RESULTS #
+        # ...IN THE APPLICATION #
+        values$DF_DRAFT_RESULTS <- TEMP_01
+        # ...IN THE GLOBAL ENVIRONMENT #
+        assign(x = paste0("DRAFT_", format(Sys.Date(), "%Y")), value = TEMP_01, envir = .GlobalEnv)
+        # ...IN THE LOCAL GIT REPOSITORY #
+        # NOTE: A MANUAL COMMIT/PUSH IS NEEDED TO SAVE TO THE GLOBAL GIT REPOSITORY #
+        updateEnvironmentDatasets(action = "push")
         
     })
 
